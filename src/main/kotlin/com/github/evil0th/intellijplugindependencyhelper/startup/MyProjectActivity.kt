fa3017fbd0dc38e58fa2fcbf0ca9ce15d96215d9
GitHub Action <action@github.com>
2025-07-21 08:28:36 +0000

    Template cleanup

--- src/main/kotlin/com/github/evil0th/intellijplugindependencyhelper/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/evil0th/intellijplugindependencyhelper/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.evil0th.intellijplugindependencyhelper.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
